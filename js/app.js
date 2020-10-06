b40c633990f7f7f31c490f8b1d69d1b07cf26467
Dmitry <dmitri.bashkin@mail.ru>
2020-10-06 17:13:38 +0300

    Inital

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var screenWidth;
+var frames = 75;
+var images = [];
+
+function preloadImages() {
+    for (i = 0; i < frames; i++) {
+        images[i] = new Image();
+        images[i].src = "../images/ural_000" + i + ".png";
+    }
+}
+
+function onMouseMove(e) {
+    var x = e.pageX;
+    var theimg = images[parseInt(x / screenWidth * frames)].src;
+    $('.image').css("background-image", "url('" + theimg + "')");
+}
+
+function onResize() {
+    screenWidth = $(document).width();
+}
+
+
+$(document).ready(preloadImages);
+$(window).on('mousemove', onMouseMove);
+$(window).resize(onResize);
+onResize();
